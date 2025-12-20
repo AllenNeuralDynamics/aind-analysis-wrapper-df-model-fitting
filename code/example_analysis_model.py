dc464890425e4399fa6323a121001df172871ed0
houhan@gmail.com <han.hou@alleninstitute.org>
2025-12-20 00:11:58 +0000

    feat: add Spec and Outputs models

--- code/example_analysis_model.py
+++ code/example_analysis_model.py
@@ -7,14 +7,15 @@ from typing import List, Optional, Union
 
 from aind_data_schema.base import GenericModel
 
-class ExampleAnalysisSpecification(GenericModel):
+class DynamicForagingModelFittingSpecification(GenericModel):
     """
     Represents the specification for an analysis, including its name,
     version, libraries to track, and parameters.
     """
 
     analysis_name: str = Field(
-        ..., description="User-defined name for the analysis"
+        default="MLE fitting",
+        description="User-defined name for the analysis"
     )
     analysis_tag: str = Field(
         ...,
@@ -22,21 +23,53 @@ class ExampleAnalysisSpecification(GenericModel):
             "User-defined tag to organize results "
             "for querying analysis output",
         ),
+
     )
-    isi_violations_cutoff: float = Field(
-        ..., description="The value to be using when filtering units by this"
+
+    # Top-level analysis parameters
+    agent_class: Optional[str] = Field(
+        default="ForagerLossCounting",
+        description="Agent class to use for model fitting",
+    )
+
+    agent_kwargs: Optional[dict] = Field(
+        default_factory=lambda: {
+            "win_stay_lose_switch": True,
+            "choice_kernel": "none",
+        },
+        description=(
+            "Top-level agent keyword arguments"
+        ),
+    )
+
+    fit_kwargs: Optional[dict] = Field(
+        default_factory=lambda: {
+            "DE_kwargs": {"polish": True, "seed": 42},
+            "k_fold_cross_validation": 10,
+        },
+        description=(
+            "Top-level fitting keyword arguments."
+        ),
     )
 
 
-class ExampleAnalysisOutputs(GenericModel):
+
+class DynamicForagingModelFittingOutputs(GenericModel):
     """
     Represents the outputs of an analysis, including a list of ISI violations.
     """
 
-    isi_violations: List[Union[str, int]] = Field(
-        ..., description="List of ISI violations detected by the analysis"
-    )
+    
     additional_info: Optional[str] = Field(
         default=None, description="Additional information about the analysis"
+    )
+
+    # Arbitrary fitting results (e.g., parameters, metrics). Stored as a dict.
+    fitting_results: Optional[dict] = Field(
+        default=None,
+        description=(
+            "Arbitrary dictionary containing fitting results such as estimated "
+            "parameters and metrics."
+        ),
     )
 
